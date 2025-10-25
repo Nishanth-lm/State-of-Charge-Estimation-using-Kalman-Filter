fda24d5b52b5039f01c12bfc08b0a573cd046f6a
Nishanth-lm <161580217+Nishanth-lm@users.noreply.github.com>
2025-10-25 15:22:43 +0200

    Add files via upload

---
+++ Soc_run.m
@@ -0,0 +1,18 @@
+open_system('BatterySOCEstimation')
+
+set_param(find_system('BatterySOCEstimation','FindAll', 'on','type','annotation','Tag','ModelFeatures'),'Interpreter','off')
+
+%% Simulation Results
+%
+% The plot below shows the real and estimated battery state-of-charge.
+%
+
+
+Soc_plot;
+
+%% Results from Real-Time Simulation
+%%
+%
+% This example has been tested on a Speedgoat Performance real-time target 
+% machine with an Intel(R) 3.5 GHz i7 multi-core CPU. This model can run 
+% in real time with a step size of 50 microseconds.
